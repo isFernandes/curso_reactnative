931c4cadb768cb5fb80003073bc07d03d0c74340
isFernandes <iago.stosfernandes@gmail.com>
2019-09-12 23:19:39 -0300

    First Commit only

---
+++ stylesMain.js
@@ -0,0 +1,24 @@
+import { StyleSheet } from 'react-native';
+
+
+const styles = StyleSheet.create({
+    container: {
+        backgroundColor: "#fff",
+        alignItems: "center",
+        justifyContent: "center",
+    },
+    bodyText: {
+        alignContent: "center",
+        backgroundColor: "#c45bc6",
+        borderRadius: 5,
+        padding: 5,
+    },
+    background: {
+        width:"100%",
+        height:"100%",
+        alignItems: "center",
+        justifyContent: "center"
+    },
+});
+
+export default styles;
