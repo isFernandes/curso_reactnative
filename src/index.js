e5e1c38f34bdc2530527c880d958b4729d6d41fb
isFernandes <iago.stosfernandes@gmail.com>
2019-09-15 20:22:57 -0300

    Curso de reactNative modificação de conteudo inserido antes

--- src/index.js
+++ src/index.js
@@ -1,27 +1,9 @@
-import React, { Fragment } from 'react';
-import {
-  StyleSheet,
-  View,
-  Text,
-  Image,
-  ImageBackground
+import React from 'react';
 
-} from 'react-native';
+import { View } from 'react-native';
 
-import styles from '../stylesMain';
+// import { Container } from './styles';
 
-const App = () => {
-  return (
-    <View >
-      <ImageBackground source={require('./assets/imgs/fundoSlide1.png')} style={styles.background}>
-        <Text style={styles.bodyText}>Slide 1 APP react Native</Text>
-      </ImageBackground>
-      <View style={styles.container}>
-        <Text style={styles.bodyText}> Hey meu mundo!! </Text>
-      </View>      
-      
-    </View>
-  );
-};
+const App = () => <View />;
 
 export default App;
